38ac106b8a7e69d86edb06f6ca91271b6eaff79b
kelwinv <kelwin08@gmail.com>
2023-06-23 18:09:33 -0300

    feature(Analytics): added Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
