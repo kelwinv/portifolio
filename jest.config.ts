0bd2fdec7bf15ba0c2c3fb2f47d9119f62a09fb5
kelwinv <kelwin08@gmail.com>
2025-03-30 23:35:21 -0300

    ci: add `test.yaml` to workflow with `jest` job

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  dir: './',
-})
+  dir: "./",
+});
 
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
   moduleNameMapper: {
-    '^app/(.*)$': '<rootDir>/app/$1',
-  }
-}
+    "^app/(.*)$": "<rootDir>/app/$1",
+  },
+};
 
-export default createJestConfig(config)
+export default createJestConfig(config);
