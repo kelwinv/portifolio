5495fdbcc064434b7a8adc916ba5304c1ea8bcbf
kelwinv <57596293+kelwinv@users.noreply.github.com>
2026-06-03 16:34:00 -0300

    Revert "Feat/new version"

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-        pathname: "/kelwinv/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
